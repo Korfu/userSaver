dc148b0be655a3d232ba1b586ff776e87c6c6017
korfu92 <konrad.korf@gmail.com>
2018-07-17 19:23:45 +0200

    ready program

--- userSaver/Program.cs
+++ userSaver/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,40 @@ namespace userSaver
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            var user = new User("","",0);
+
+            Console.WriteLine("Name:");
+            user.Name = Console.ReadLine();
+
+            Console.WriteLine("Last name:");
+            user.Surname = Console.ReadLine();
+
+            Console.WriteLine("Born in (year):");
+            user.Age = DateTime.Today.Year - Int32.Parse(Console.ReadLine());
+           
+
+            string json = JsonConvert.SerializeObject(user, Formatting.Indented);
+            Console.Write(json);
+            Console.ReadKey();
         }
+
+     
+    }
+
+    public class User
+    {
+        public User(string firstname,string lastname,int dob)
+        {
+            Name = firstname;
+            Surname = lastname;
+            Age = dob;
+        }
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        private int born;
     }
 }
